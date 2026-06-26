5f252269dbaa0f88fb5f5e42152429bef9282ebd
hassan-khattak <h.hassanzameerkk@gmail.com>
2026-06-27 03:19:28 +0500

    fix(api): wrap user save + TeamGeneratorService in transaction to prevent orphaned users

--- api/app/controllers/api/v1/auth/registrations_controller.rb
+++ api/app/controllers/api/v1/auth/registrations_controller.rb
@@ -7,14 +7,16 @@ module Api
         def create
           user = User.new(registration_params)
 
-          if user.save
+          ActiveRecord::Base.transaction do
+            user.save!
             TeamGeneratorService.call(user)
-            access_token   = user.generate_jwt
-            refresh_token  = RefreshTokenService.issue(user)
-            render json: token_response(access_token, refresh_token), status: :created
-          else
-            render json: { errors: user.errors.full_messages }, status: :unprocessable_content
           end
+
+          access_token  = user.generate_jwt
+          refresh_token = RefreshTokenService.issue(user)
+          render json: token_response(access_token, refresh_token), status: :created
+        rescue ActiveRecord::RecordInvalid
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_content
         end
 
         private
